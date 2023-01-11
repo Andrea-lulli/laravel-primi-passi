32adb99b73cf87c168f66101ca3c8dbb2190b528
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-01-11 16:14:08 +0100

    esercizio completo

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>contact</h1>
+    <p>{{$contact}}</p>
+
+    <div>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/blog">Blog</a>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Boolean';
+    return view('home',compact('name'));
+});
+
+Route::get('/contact', function () {
+    $contact = '147896';
+    return view('contact', compact('contact'));
+});
+
+Route::get('/blog', function () {
+    
+    return view('blog');
 });
